3bc01bd88c9bc5db9b699e419c5ec5372d21c9c6
mkhoatd <binnhatle@gmail.com>
2022-07-06 22:14:23 +0700

    downgrade angular

--- DatingAppAPI/Program.cs
+++ DatingAppAPI/Program.cs
@@ -43,7 +43,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
