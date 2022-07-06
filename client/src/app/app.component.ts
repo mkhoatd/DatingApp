5538dccefbf915de2a3e4bee512b7aba925db2cb
mkhoatd <binnhatle@gmail.com>
2022-07-06 20:19:07 +0700

    nothing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The Dating app';
+  user: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.
+  }
 }
