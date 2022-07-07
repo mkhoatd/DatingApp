8069cf32863ccbdb15b612881eb0bb2a74598e07
mkhoatd <binnhatle@gmail.com>
2022-07-07 08:33:57 +0700

    add user password

--- DatingAppAPI/Data/Migrations/20220707013312_add-user-password.Designer.cs
+++ DatingAppAPI/Data/Migrations/20220707013312_add-user-password.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DatingAppAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingAppAPI.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220707013312_add-user-password")]
+    partial class adduserpassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.6");
