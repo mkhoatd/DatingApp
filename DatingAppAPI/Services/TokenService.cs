fb1b95e763758bbd0dcdddb4e354dea2fce0e363
mkhoatd <binnhatle@gmail.com>
2022-07-07 10:14:18 +0700

    Add logger TokenService.cs

--- DatingAppAPI/Services/TokenService.cs
+++ DatingAppAPI/Services/TokenService.cs
@@ -14,16 +14,19 @@ namespace DatingAppAPI.Services
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
-        public TokenService(IConfiguration config)
+        private readonly ILogger<TokenService> _logger;
+        public TokenService(IConfiguration config, ILogger<TokenService> logger)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+            _logger = logger;
         }
         public string CreateToken(AppUser user)
         {
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName),
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
             };
+            _logger.LogDebug("Creating token for user {user.UserName} with claim {claims}", user.UserName, claims);
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -31,8 +34,10 @@ namespace DatingAppAPI.Services
                 Expires = DateTime.Now.AddDays(777),
                 SigningCredentials = creds
             };
+            _logger.LogDebug("Token descriptor for user {user.UserName}: {tokenDescriptor}", user.UserName, tokenDescriptor);
             var tokenHandler = new JwtSecurityTokenHandler();
             var token = tokenHandler.CreateToken(tokenDescriptor);
+            _logger.LogDebug("Token for user {user.UserName}: {token}", user.UserName, token);
             return tokenHandler.WriteToken(token);
         }
     }
